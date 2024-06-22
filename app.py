982f8b72d7ed084f99ee5b205e6fcc3c6e858313
Charlie1128 <48147950+Tokutake1128@users.noreply.github.com>
2024-06-22 10:18:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,15 @@ import pillow_heif
 import zipfile
 import io
 
-# Pillow-HEIFの設定
 pillow_heif.register_heif_opener()
 
 def convert_heif_to_jpeg(file):
-    # HEIF/HEICファイルを読み込む
+    # HEIF/HEICファイルの読み込み
     image = Image.open(file)
     return image
 
 st.title("HEIF/HEIC to JPEG Converter")
 
-# 複数のHEIF/HEICファイルをアップロード
 uploaded_files = st.file_uploader("Drop HEIF/HEIC files", type=["heif", "heic"], accept_multiple_files=True)
 st.write("ドラッグ&ドロップで複数ファイルをアップロードできます")
 
@@ -25,25 +23,23 @@ if uploaded_files:
         for uploaded_file in uploaded_files:
             image = convert_heif_to_jpeg(uploaded_file)
             
-            # JPEGファイルに変換
+            # JPEG変換
             img_byte_arr = io.BytesIO()
             image.save(img_byte_arr, format="JPEG", quality=95)
             img_byte_arr.seek(0)
             
-            # ファイル名を変更してZIPに追加
+            # ZIP
             zip_file.writestr(f"{uploaded_file.name.split('.')[0]}.jpeg", img_byte_arr.read())
 
     zip_buffer.seek(0)
     
-    # ZIPファイルをダウンロード
+    # ZIPダウンロード
     st.download_button(
         label="Download ZIP",
         data=zip_buffer,
         file_name="converted_images.zip",
         mime="application/zip",
     )
-
-# カスタムCSSとHTMLを追加してフッターを作成
 footer = """
     <style>
     .footer {
